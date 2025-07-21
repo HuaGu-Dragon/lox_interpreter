1cd37b199e8ec8020b12d3563e9104ece045b57f
俺不叫化咕龙 <1801943622@qq.com>
2025-07-21 19:18:01 +0800

    Refactor Token structure and update Lexer to use TokenKind enum, may be a few ugly now qwq.

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,13 @@ use std::fmt::Display;
 
 use miette::{Error, LabeledSpan, WrapErr, miette};
 
+pub struct Token<'de> {
+    kind: TokenKind,
+    literal: &'de str,
+}
+
 #[derive(Debug, Clone, PartialEq)]
-pub enum Token<'de> {
+pub enum TokenKind {
     LeftParen,
     RightParen,
     LeftBrace,
@@ -25,9 +30,9 @@ pub enum Token<'de> {
     Slash,
     Bang,
     Equal,
-    String(&'de str),
-    Ident(&'de str),
-    Number(&'de str, f64),
+    String,
+    Ident,
+    Number(f64),
     And,
     Class,
     Else,
@@ -47,44 +52,45 @@ pub enum Token<'de> {
 
 impl Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Token::LeftParen => write!(f, "LEFT_PAREN ( null"),
-            Token::RightParen => write!(f, "RIGHT_PAREN ) null"),
-            Token::LeftBrace => write!(f, "LEFT_BRACE {{ null"),
-            Token::RightBrace => write!(f, "RIGHT_BRACE }} null"),
-            Token::Comma => write!(f, "COMMA , null"),
-            Token::Dot => write!(f, "DOT . null"),
-            Token::Minus => write!(f, "MINUS - null"),
-            Token::Plus => write!(f, "PLUS + null"),
-            Token::Semicolon => write!(f, "SEMICOLON ; null"),
-            Token::Star => write!(f, "STAR * null"),
-            Token::BangEqual => write!(f, "BANG_EQUAL != null"),
-            Token::EqualEqual => write!(f, "EQUAL_EQUAL == null"),
-            Token::GreaterEqual => write!(f, "GREATER_EQUAL >= null"),
-            Token::LessEqual => write!(f, "LESS_EQUAL <= null"),
-            Token::Greater => write!(f, "GREATER > null"),
-            Token::Less => write!(f, "LESS < null"),
-            Token::Slash => write!(f, "SLASH / null"),
-            Token::Bang => write!(f, "BANG ! null"),
-            Token::Equal => write!(f, "EQUAL = null"),
-            Token::String(_) => todo!(),
-            Token::Ident(i) => write!(f, "IDENTIFIER {i} null"),
-            Token::Number(lit, n) => write!(f, "NUMBER {lit} {n}"),
-            Token::And => write!(f, "AND and null"),
-            Token::Class => write!(f, "CLASS class null"),
-            Token::Else => write!(f, "ELSE else null"),
-            Token::False => write!(f, "FALSE false null"),
-            Token::For => write!(f, "FOR for null"),
-            Token::Fun => write!(f, "FUN fun null"),
-            Token::If => write!(f, "IF if null"),
-            Token::Nil => write!(f, "NIL nil null"),
-            Token::Or => write!(f, "OR or null"),
-            Token::Return => write!(f, "RETURN return null"),
-            Token::Super => write!(f, "SUPER super null"),
-            Token::This => write!(f, "THIS this null"),
-            Token::True => write!(f, "TRUE true null"),
-            Token::Var => write!(f, "VAR var null"),
-            Token::While => write!(f, "WHILE while null"),
+        let lit = self.literal;
+        match self.kind {
+            TokenKind::LeftParen => write!(f, "LEFT_PAREN {lit} null"),
+            TokenKind::RightParen => write!(f, "RIGHT_PAREN {lit} null"),
+            TokenKind::LeftBrace => write!(f, "LEFT_BRACE {lit} null"),
+            TokenKind::RightBrace => write!(f, "RIGHT_BRACE {lit} null"),
+            TokenKind::Comma => write!(f, "COMMA {lit} null"),
+            TokenKind::Dot => write!(f, "DOT {lit} null"),
+            TokenKind::Minus => write!(f, "MINUS {lit} null"),
+            TokenKind::Plus => write!(f, "PLUS {lit} null"),
+            TokenKind::Semicolon => write!(f, "SEMICOLON {lit} null"),
+            TokenKind::Star => write!(f, "STAR {lit} null"),
+            TokenKind::BangEqual => write!(f, "BANG_EQUAL {lit} null"),
+            TokenKind::EqualEqual => write!(f, "EQUAL_EQUAL {lit} null"),
+            TokenKind::GreaterEqual => write!(f, "GREATER_EQUAL {lit} null"),
+            TokenKind::LessEqual => write!(f, "LESS_EQUAL {lit} null"),
+            TokenKind::Greater => write!(f, "GREATER {lit} null"),
+            TokenKind::Less => write!(f, "LESS {lit} null"),
+            TokenKind::Slash => write!(f, "SLASH {lit} null"),
+            TokenKind::Bang => write!(f, "BANG {lit} null"),
+            TokenKind::Equal => write!(f, "EQUAL {lit} null"),
+            TokenKind::String => todo!(),
+            TokenKind::Ident => write!(f, "IDENTIFIER {lit} null"),
+            TokenKind::Number(n) => write!(f, "NUMBER {lit} {n}"),
+            TokenKind::And => write!(f, "AND {lit} null"),
+            TokenKind::Class => write!(f, "CLASS {lit} null"),
+            TokenKind::Else => write!(f, "ELSE {lit} null"),
+            TokenKind::False => write!(f, "FALSE {lit} null"),
+            TokenKind::For => write!(f, "FOR {lit} null"),
+            TokenKind::Fun => write!(f, "FUN {lit} null"),
+            TokenKind::If => write!(f, "IF {lit} null"),
+            TokenKind::Nil => write!(f, "NIL {lit} null"),
+            TokenKind::Or => write!(f, "OR {lit} null"),
+            TokenKind::Return => write!(f, "RETURN {lit} null"),
+            TokenKind::Super => write!(f, "SUPER {lit} null"),
+            TokenKind::This => write!(f, "THIS {lit} null"),
+            TokenKind::True => write!(f, "TRUE {lit} null"),
+            TokenKind::Var => write!(f, "VAR {lit} null"),
+            TokenKind::While => write!(f, "WHILE {lit} null"),
         }
     }
 }
@@ -114,29 +120,36 @@ impl<'de> Iterator for Lexer<'de> {
         self.rest = chars.as_str();
         self.byte += c.len_utf8();
 
-        enum Start<'de> {
+        enum Start {
             String,
             Ident,
             Number,
-            IfEqualElse(Token<'de>, Token<'de>),
+            IfEqualElse(TokenKind, TokenKind),
         }
 
+        let process = |kind: TokenKind| {
+            Some(Ok(Token {
+                kind,
+                literal: &self.whole[self.byte - c.len_utf8()..self.byte],
+            }))
+        };
+
         let started = match c {
-            '(' => return Some(Ok(Token::LeftParen)),
-            ')' => return Some(Ok(Token::RightParen)),
-            '{' => return Some(Ok(Token::LeftBrace)),
-            '}' => return Some(Ok(Token::RightBrace)),
-            ',' => return Some(Ok(Token::Comma)),
-            '.' => return Some(Ok(Token::Dot)),
-            '-' => return Some(Ok(Token::Minus)),
-            '+' => return Some(Ok(Token::Plus)),
-            ';' => return Some(Ok(Token::Semicolon)),
-            '*' => return Some(Ok(Token::Star)),
-            '/' => return Some(Ok(Token::Slash)),
-            '!' => Start::IfEqualElse(Token::BangEqual, Token::Bang),
-            '=' => Start::IfEqualElse(Token::EqualEqual, Token::Equal),
-            '>' => Start::IfEqualElse(Token::GreaterEqual, Token::Greater),
-            '<' => Start::IfEqualElse(Token::LessEqual, Token::Less),
+            '(' => return process(TokenKind::LeftParen),
+            ')' => return process(TokenKind::RightParen),
+            '{' => return process(TokenKind::LeftBrace),
+            '}' => return process(TokenKind::RightBrace),
+            ',' => return process(TokenKind::Comma),
+            '.' => return process(TokenKind::Dot),
+            '-' => return process(TokenKind::Minus),
+            '+' => return process(TokenKind::Plus),
+            ';' => return process(TokenKind::Semicolon),
+            '*' => return process(TokenKind::Star),
+            '/' => return process(TokenKind::Slash),
+            '!' => Start::IfEqualElse(TokenKind::BangEqual, TokenKind::Bang),
+            '=' => Start::IfEqualElse(TokenKind::EqualEqual, TokenKind::Equal),
+            '>' => Start::IfEqualElse(TokenKind::GreaterEqual, TokenKind::Greater),
+            '<' => Start::IfEqualElse(TokenKind::LessEqual, TokenKind::Less),
             'a'..='z' | 'A'..='Z' | '_' => Start::Ident,
             '0'..='9' => Start::Number,
             '"' => Start::String,
@@ -160,9 +173,15 @@ impl<'de> Iterator for Lexer<'de> {
                 if self.rest.starts_with('=') {
                     self.rest = &self.rest[1..];
                     self.byte += 1;
-                    return Some(Ok(yes));
+                    return Some(Ok(Token {
+                        kind: yes,
+                        literal: &self.whole[self.byte - 2..self.byte],
+                    }));
                 } else {
-                    return Some(Ok(no));
+                    return Some(Ok(Token {
+                        kind: no,
+                        literal: &self.whole[self.byte - 1..self.byte],
+                    }));
                 }
             }
         }
