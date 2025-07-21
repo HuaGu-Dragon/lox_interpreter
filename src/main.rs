2f24b73e793973056dc4596875ff0b3461f0b0da
俺不叫化咕龙 <1801943622@qq.com>
2025-07-21 18:55:38 +0800

    Update token output format and add expected token comments in main function

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,49 @@ fn main() -> miette::Result<()> {
 
             for token in Lexer::new(&file_contents) {
                 let token = token?;
-                println!("{token}");
+                println!("// expect: {token}");
             }
+            println!("// expect: EOF  null");
         }
     }
     Ok(())
 }
+// expect: LEFT_PAREN ( null
+// expect: RIGHT_PAREN ) null
+// expect: LEFT_BRACE { null
+// expect: RIGHT_BRACE } null
+// expect: SEMICOLON ; null
+// expect: COMMA , null
+// expect: PLUS + null
+// expect: MINUS - null
+// expect: STAR * null
+// expect: BANG_EQUAL != null
+// expect: EQUAL_EQUAL == null
+// expect: LESS_EQUAL <= null
+// expect: GREATER_EQUAL >= null
+// expect: BANG_EQUAL != null
+// expect: LESS < null
+// expect: GREATER > null
+// expect: SLASH / null
+// expect: DOT . null
+// expect: EOF  null
+
+// expect: LEFT_PAREN ( null
+// expect: RIGHT_PAREN ) null
+// expect: LEFT_BRACE { null
+// expect: RIGHT_BRACE } null
+// expect: SEMICOLON ; null
+// expect: COMMA , null
+// expect: PLUS + null
+// expect: MINUS - null
+// expect: STAR * null
+// expect: BANG_EQUAL != null
+// expect: EQUAL_EQUAL == null
+// expect: LESS_EQUAL <= null
+// expect: GREATER_EQUAL >= null
+// expect: BANG_EQUAL != null
+// expect: LESS < null
+// expect: GREATER > null
+// expect: SLASH / null
+// expect: DOT . null
+// expect: EOF  null
